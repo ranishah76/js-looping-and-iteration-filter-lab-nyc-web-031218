b61092cb4dd575e51da7e8c9aa26610637070687
Rani Shah <rani.shah@flatironschool.com>
2018-04-23 23:56:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file
+function findMatching (drivers, name) {
+  return drivers.filter(function (dname) {
+    return dname.toLowerCase() == name.toLowerCase();
+  })
+}
+
+function fuzzyMatch(array, string) {
+  return array.filter(function (name) {
+    return name[0].toLowerCase() === string[0].toLowerCase();
+  })
+}
+
+function matchName (drivers, string) {
+  return drivers.filter(function (driver) {
+    return driver.name.toLowerCase() === string.toLowerCase()
+  })
+}
